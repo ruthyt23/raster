51dfcd48c55c8528e4fda4bf8f3b061c591a344e
Ruth Taddesse <ruth.y.taddesse@gmail.com>
2024-07-01 15:29:13 +0000

    #1: Grayscale

--- src/grayscale.ml
+++ src/grayscale.ml
@@ -1,9 +1,30 @@
 open Core
 
-(* You need to change the implementation of this function so that it does something
-   to the image instead of just leaving it untouched. *)
+(* You need to change the implementation of this function so that it does
+   something to the image instead of just leaving it untouched. *)
 let transform image =
-  image
+  Image.map image ~f:(fun (r, g, b) ->
+    let avg = (r + g + b) / 3 in
+    avg, avg, avg)
+;;
+
+let%expect_test "transform" =
+  let beach = Image.load_ppm ~filename:"../images/beach_portrait.ppm" in
+  let gray_beach = transform beach in
+  let ref_gray_beach =
+    Image.load_ppm ~filename:"../images/reference-beach_portrait_gray.ppm"
+  in
+  let pixels_wrong =
+    Image.foldi gray_beach ~init:0 ~f:(fun ~x ~y acc _ ->
+      if Pixel.equal
+           (Image.get gray_beach ~x ~y)
+           (Image.get ref_gray_beach ~x ~y)
+      then acc
+      else acc + 1)
+  in
+  match pixels_wrong with
+  | 0 -> ()
+  | _ -> printf "Test failed: # of pixels that differ = %d" pixels_wrong
 ;;
 
 let command =
@@ -20,5 +41,6 @@ let command =
         let image = Image.load_ppm ~filename |> transform in
         Image.save_ppm
           image
-          ~filename:(String.chop_suffix_exn filename ~suffix:".ppm" ^ "_gray.ppm")]
+          ~filename:
+            (String.chop_suffix_exn filename ~suffix:".ppm" ^ "_gray.ppm")]
 ;;
